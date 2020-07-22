ae83a5964e4800b27f463d2d5d27abdd0265293d
ersaumya <ersaumyarout@gmail.com>
2020-07-22 20:43:08 +0530

    child route config

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,26 +1,42 @@
-import { CourseResolver } from './services/course.resolver';
-import { CourseComponent } from './course/course.component';
-import { HomeComponent } from './home/home.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { LessonsResolver } from './services/lessons.resolver';
+import { LessonsListComponent } from "./lessons-list/lessons-list.component";
+import { LessonDetailComponent } from "./lesson/lesson-detail.component";
+import { CourseResolver } from "./services/course.resolver";
+import { CourseComponent } from "./course/course.component";
+import { HomeComponent } from "./home/home.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-    path:"",component:HomeComponent
+    path: "",
+    component: HomeComponent,
   },
   {
-    path:":courseUrl",
-    component:CourseComponent,
-    resolve:{
-      course:CourseResolver
-    }
-  }
+    path: ":courseUrl",
+    component: CourseComponent,
+    children: [
+      {
+        path: "",
+        component: LessonsListComponent,
+        resolve:{
+          lessons:LessonsResolver
+        }
+      },
+      {
+        path: "lessons/:lessonSeqNo",
+        component: LessonDetailComponent,
+      },
+    ],
+    resolve: {
+      course: CourseResolver,
+    },
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  providers: [CourseResolver],
+  providers: [CourseResolver,LessonsResolver],
 })
 export class CoursesRoutingModule {}
---
+++ src/app/courses/services/lessons.resolver.ts
@@ -0,0 +1,19 @@
+import { CoursesService } from './courses.service';
+import { LessonSummary } from './../model/lesson-summary';
+import { Injectable } from "@angular/core";
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from "@angular/router";
+import { Observable } from "rxjs";
+
+
+@Injectable()
+export class LessonsResolver implements Resolve<LessonSummary[]> {
+    constructor(private courses:CoursesService){}
+    resolve(route: ActivatedRouteSnapshot,state: RouterStateSnapshot): Observable<LessonSummary[]> {
+      const courseUrl=route.paramMap.get("courseUrl");
+      return this.courses.loadAllCourseLessonsSummary(courseUrl);
+    }
+}
